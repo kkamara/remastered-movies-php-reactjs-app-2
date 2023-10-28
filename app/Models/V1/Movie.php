5e7eff3611b53222ab1d8316f1b7e6b45514cd41
kkamara <kamaracomputers@gmail.com>
2023-10-28 15:06:24 +0100

    Add movie resources in backend PHP

---
+++ app/Models/V1/Movie.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\V1;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    public function search() {
+        
+    }
+}
